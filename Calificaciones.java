aca0026597b1c57b8dda14ff60163547a9c80c10
Elder-V <vas26424@uvg.edu.gt>
2026-08-26 09:08:37 -0600

    Primera parte de Calificaciones

--- Calificaciones.java
+++ Calificaciones.java
@@ -1,3 +1,33 @@
 public class Calificaciones {
+    protected int cantidadCalificaciones;
+    protected int cantidadDispo;
+    protected int registrocalificaciones;
+    protected int limitecalificaciones;
+    protected int calificaciones;
+    protected Boolean espacioDis;
+
+    public void registrarCalificacion(int cantidadCalificaciones) {
+        this.cantidadCalificaciones = cantidadCalificaciones;
+    }
     
-}
+    public void dentrodellimite(int cantidadCalificaciones) {
+        this.limitecalificaciones = cantidadCalificaciones;
+        if (limitecalificaciones > 10) {
+            System.out.println("Pasaste el limite de calificaciones");
+        }
+        else{
+            System.out.println("Calificacion ingresada");
+         } 
+        }
+
+    public void calificaciones(int calificaciones){
+        this.calificaciones = calificaciones;
+    }
+
+    public void espacioDisponible(Boolean calificaciones){
+        this.espacioDis = calificaciones;
+
+        } 
+    }
+
+
