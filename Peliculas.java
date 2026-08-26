33d3e50dc51fbf26e223e5125b78898396e64c7d
fstgxpl4y <fstgxpl4y@gmail.com>
2026-08-26 09:15:00 -0600

    Clase Peliculas hecha

--- Peliculas.java
+++ Peliculas.java
@@ -1,3 +1,40 @@
 public class Peliculas {
-    
+    private int calificacion;
+    private String pelicula;
+    private String namePelicula;
+    private String idPelicula;
+    private int numeroPelicula;
+
+    public void pelicula(String pelicula) {
+        this.namePelicula = pelicula;
+    }
+
+    public void pelicula(String idPelicula, int numeroPelicula) {
+        this.namePelicula = idPelicula;
+        this.numeroPelicula = numeroPelicula;
+    }
+
+    public void calificado(int critica) {
+        this.calificacion = critica;
+    }
+
+    public String getNamePelicula() {
+        return namePelicula;
+    }
+
+    public String Pelicula() {
+        return pelicula;
+    }
+
+    public String getIdPelicula() {
+        return idPelicula;
+    }
+
+    public int getNumeroPelicula() {
+        return numeroPelicula;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
 }
