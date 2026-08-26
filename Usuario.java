e9562afe9c850de8e88648051acbd721d616be39
Elder-V <vas26424@uvg.edu.gt>
2026-08-26 08:34:08 -0600

    Clase Usuario hecha

--- Usuario.java
+++ Usuario.java
@@ -1,3 +1,19 @@
 public class Usuario {
+    protected String nombre;
+    protected String usuario;
+    protected int edad;
+    protected int calificaciones;
+
+    public void nombreUsuario(String usuario) {
+        this.nombre = usuario;
+    }
     
+    public void nombreUsuario(String usuario, int edad) {
+        this.nombre = usuario;
+        this.edad = edad;
+    }
+
+    public void calificaciones(int calificaciones){
+        this.calificaciones = calificaciones;
+    }
 }
